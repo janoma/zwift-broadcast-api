644b4a11b5d60a969eb04331faaef75459e16076
Alejandro Mallea <janoma@gmail.com>
2025-02-19 22:30:08 -0800

    build: remove globals from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
@@ -6,7 +5,6 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["./src/**/*.ts"] },
   { ignores: ["dist", "node_modules", "**/*.js"] },
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
 ];
