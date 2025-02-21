12798ce933362f8a7e398a7fd14f004de9eb8981
Alejandro Mallea <janoma@gmail.com>
2025-02-20 19:23:15 -0800

    build: fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,12 @@ export default [
   { ignores: ["dist", "node_modules", "**/*.js"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 ];
