54e7cd7331a4c347686761958d77c60672f76e65
Alejandro Mallea <janoma@gmail.com>
2025-02-20 19:24:05 -0800

    build: ignore eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["./src/**/*.ts"] },
-  { ignores: ["dist", "node_modules", "**/*.js"] },
+  { ignores: ["dist", "node_modules", "**/*.js", "**/*.mjs"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   {
